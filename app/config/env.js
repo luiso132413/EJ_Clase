86f11e84574f043e5a89f74efcf3fb0fa8121d49
luiso132413 <loviedog@miumg.edu.gt>
2024-08-26 20:46:52 -0600

    segundo cambio

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,10 @@
 
 
 const env = {
-  database: 'antigua_umg2024_4441',
-  username: 'antigua_umg2024_4441_user',
-  password: 'nmSiYN0EyxR4gUX5vY0yQOKgHLxqBDW8',
-  host: 'dpg-cqc5qld6l47c73cufe6g-a',
+  database: 'antigua_umg2024_4441_user',
+  username: 'antigua_umg2024_4441_user_user',
+  password: 'tYnVvGiYQ0Leg6257ZJRhaT17J7Yw6I2',
+  host: 'dpg-cr6jnbt6l47c7394rch0-a',
   dialect: 'postgres',
   /*database: 'postgres',
   username: 'postgres',
